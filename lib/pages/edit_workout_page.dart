3b8de8205052ee962cf3cb78fa9835f502b22ea6
Bradley Thater <bradleythater@gmail.com>
2024-02-15 17:14:18 -0800

    Added stubs for create/edit workout pages

---
+++ lib/pages/edit_workout_page.dart
@@ -0,0 +1,5 @@
+/**
+ * This page is intended to allow a user to edit a workout that is either in in the future, in progress, or complete. 
+ * There is also a plan to have a page to pick and choose what type of workouts you do, that would be its own seperate page.
+ * Things you should be able to set: rep count, set count, rep weight, type of set, triggers, power rank
+ */
